0f54bd59c5cb8b15b12bddab250091f3c409314f
patrick <patrick@kappa-mm.de>
2021-06-19 13:50:45 +0200

    Update timeout

--- utils/utils_quic.go
+++ utils/utils_quic.go
@@ -76,7 +76,7 @@ func GRPCDialer(pconn *net.UDPConn) (dialer Dialer) {
 
 		sess, err := quic.DialContext(ctx, pconn, rAddr, "", tlsConf, &quic.Config{
 			KeepAlive:      true,
-			MaxIdleTimeout: time.Millisecond * 2000,
+			MaxIdleTimeout: time.Millisecond * 3200,
 		})
 		if err != nil {
 			return
