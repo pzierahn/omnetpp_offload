6111fbb8dd97a096b516c4cc9c78db4297df671e
patrick <patrick@kappa-mm.de>
2021-07-15 21:07:48 +0200

    Check in.

--- cmd/stargate/stargate.go
+++ cmd/stargate/stargate.go
@@ -17,35 +17,33 @@ func main() {
 	go func(inx int) {
 		defer wg.Done()
 
-		ctx, cnl := context.WithTimeout(context.Background(), time.Second*5)
-		defer cnl()
+		//ctx, cnl := context.WithTimeout(context.Background(), time.Second*5)
+		//defer cnl()
+		ctx := context.Background()
 
 		conn, peer, err := stargate.DialUDP(ctx, "123456")
 		if err != nil {
-			log.Println(err)
+			log.Printf("error %d: %v", inx, err)
 			return
 		}
 
 		log.Printf("Connected %d: local=%v peer=%v", inx, conn.LocalAddr(), peer)
 
-		//wait := time.Second * 10
-		//log.Printf("Start waiting for %v...", wait)
-		//time.Sleep(wait)
-		//log.Printf("Done waiting")
-		//
-		//conn.WriteToUDP()
+		wait := time.Second * 10
+		time.Sleep(wait)
+		log.Printf("Writing stuff...")
 
 		_, err = conn.WriteTo([]byte("Pups"), peer)
 		if err != nil {
-			log.Printf("Connected %d: %v", inx, err)
+			log.Printf("error %d: %v", inx, err)
 		}
 
 	}(1)
 
-	//wait := time.Second * 10
-	//log.Printf("Start waiting for %v...", wait)
-	//time.Sleep(wait)
-	//log.Printf("Done waiting")
+	wait := time.Second * 1
+	log.Printf("Start waiting for %v...", wait)
+	time.Sleep(wait)
+	log.Printf("Done waiting")
 
 	wg.Add(1)
 	go func(inx int) {
@@ -56,7 +54,7 @@ func main() {
 
 		conn, peer, err := stargate.DialUDP(ctx, "123456")
 		if err != nil {
-			log.Println(err)
+			log.Printf("error %d: %v", inx, err)
 			return
 		}
 
@@ -65,24 +63,11 @@ func main() {
 		buf := make([]byte, 1024)
 		br, err := conn.Read(buf)
 		if err != nil {
-			log.Printf("Connected %d: %v", inx, err)
+			log.Printf("error %d: %v", inx, err)
 		}
 
 		log.Printf("Connected %d: read %s", inx, string(buf[:br]))
 	}(2)
-
-	//wg.Add(1)
-	//go func(inx int) {
-	//	defer wg.Done()
-	//
-	//	conn, remote, err := stargate.DialUDP(context.Background(), "123456")
-	//	if err != nil {
-	//		log.Println(err)
-	//		return
-	//	}
-	//
-	//	log.Printf("Connect %d: local=%v remote=%v", inx, conn.LocalAddr(), remote)
-	//}(3)
 
 	wg.Wait()
 }
