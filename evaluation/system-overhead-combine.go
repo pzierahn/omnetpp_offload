3efeed6c75131dca468d212cc2d9299c40c8cb24
patrick <patrick@kappa-mm.de>
2021-09-08 14:20:47 +0200

    Check in.

--- evaluation/system-overhead-combine.go
+++ evaluation/system-overhead-combine.go
@@ -9,56 +9,56 @@ import (
 
 func main() {
 
-	start := ""
-	out := "system-overhead.csv"
-	dir := "system-overhead-scenarios"
+	//dir := "system-overhead-scenarios"
+	//combine := map[string]string{
+	//	"system-overhead.csv": "",
+	//}
 
-	//dir := "system-overhead-data"
-
-	//start := "opp-edge-eval-setup"
-	//out := "opp-edge-eval-setup.csv"
-
-	//start := "opp-edge-eval-transfers"
-	//out := "opp-edge-eval-transfers.csv"
-
-	//start := "opp-edge-eval-runs"
-	//out := "opp-edge-eval-runs.csv"
+	dir := "system-overhead-data"
+	combine := map[string]string{
+		//"system-overhead.csv":         "",
+		"opp-edge-eval-setup.csv":     "opp-edge-eval-setup",
+		"opp-edge-eval-transfers.csv": "opp-edge-eval-transfers",
+		"opp-edge-eval-runs.csv":      "opp-edge-eval-runs",
+	}
 
 	files, err := os.ReadDir(dir)
 	if err != nil {
 		panic(err)
 	}
 
-	var entries []string
-	var inx int
+	for csvname, start := range combine {
+		var entries []string
+		var inx int
 
-	for _, file := range files {
+		for _, file := range files {
 
-		if !strings.HasPrefix(file.Name(), start) {
-			continue
+			if !strings.HasPrefix(file.Name(), start) {
+				continue
+			}
+
+			byt, err := os.ReadFile(filepath.Join(dir, file.Name()))
+			if err != nil {
+				panic(err)
+			}
+
+			txt := string(byt)
+			txt = strings.TrimSpace(txt)
+			lines := strings.Split(txt, "\n")
+
+			if inx == 0 {
+				entries = append(entries, lines...)
+			} else {
+				entries = append(entries, lines[1:]...)
+			}
+
+			inx++
 		}
 
-		byt, err := os.ReadFile(filepath.Join(dir, file.Name()))
+		data := strings.Join(entries, "\n") + "\n"
+		err = ioutil.WriteFile(csvname, []byte(data), 0755)
 		if err != nil {
 			panic(err)
 		}
-
-		txt := string(byt)
-		txt = strings.TrimSpace(txt)
-		lines := strings.Split(txt, "\n")
-
-		if inx == 0 {
-			entries = append(entries, lines...)
-		} else {
-			entries = append(entries, lines[1:]...)
-		}
-
-		inx++
-	}
-
-	data := strings.Join(entries, "\n") + "\n"
-	err = ioutil.WriteFile(out, []byte(data), 0755)
-	if err != nil {
-		panic(err)
 	}
 }
