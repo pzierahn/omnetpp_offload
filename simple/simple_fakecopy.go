01bfa96fca63ffdab42c83e036d7ca8e4618a965
patrick <patrick@kappa-mm.de>
2021-09-17 20:23:34 +0200

    Performance improvements.

--- simple/simple_fakecopy.go
+++ simple/simple_fakecopy.go
@@ -16,7 +16,7 @@ func FakeCopy(source, target string) (err error) {
 		return
 	}
 
-	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+	err = filepath.WalkDir(source, func(path string, info os.DirEntry, err error) error {
 
 		if err != nil {
 			return err
