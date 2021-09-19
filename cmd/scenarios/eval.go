e70889be08b7de852ac50ef74a2e0a7b61482d7a
patrick <patrick@kappa-mm.de>
2021-09-19 14:17:31 +0200

    Parse flags.

--- cmd/scenarios/eval.go
+++ cmd/scenarios/eval.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
+	"flag"
 	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"golang.org/x/crypto/ssh"
+	"google.golang.org/grpc/benchmark/flags"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strings"
 	"time"
@@ -17,15 +21,33 @@ import (
 
 const (
 	projectDir = "~/patrick/project.go.omnetpp"
+	simulation = "/home/fioo/patrick/tictoc"
+	broker     = "85.214.35.83"
 )
 
 var (
 	cli       *client.Client
 	sshClient *ssh.Client
+	dockerId  string
 )
 
-func init() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+var (
+	jobNums  = []int{1, 2, 4, 6, 8}
+	repeat   = 5
+	connects = []string{"p2p", "relay"}
+)
+
+func initDockerSSH() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	go func() {
+		select {
+		case rec := <-sig:
+			log.Printf("received SIGTERM %v\n", rec)
+			stop(dockerId)
+			os.Exit(1)
+		}
+	}()
 
 	var err error
 	cli, err = client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
@@ -90,15 +112,14 @@ func updateRepo() {
 	}
 }
 
-func runScenario(name, connect string, repeat int) {
+func runScenario(scenario, connect string, trail int) (duration time.Duration, err error) {
 
-	log.Printf("\n\n##################################################")
-	log.Printf("Staring scenario %s %s, %d", name, connect, repeat)
-	log.Printf("##################################################\n\n")
+	log.Printf("Staring scenario scenario=%s connect=%s trail=%d", scenario, connect, trail)
 
 	session, err := sshClient.NewSession()
 	if err != nil {
-		log.Fatalf("unable to create a new session: %s", err)
+		err = fmt.Errorf("unable to create a new session: %s", err)
+		return
 	}
 	defer func() {
 		_ = session.Close()
@@ -107,17 +128,49 @@ func runScenario(name, connect string, repeat int) {
 	session.Stdout = os.Stdout
 	session.Stderr = os.Stderr
 
-	//cmd := "cd " + projectDir + ";" +
-	//	"go run cmd/scenario/scenario.go -s "
+	// session.Setenv() doesn't work.
+	// see https://vic.demuzere.be/articles/environment-variables-setenv-ssh-golang/
+	envVars := []string{
+		fmt.Sprintf("SCENARIOID=%s", scenario),
+		fmt.Sprintf("CONNECT=%s", connect),
+		fmt.Sprintf("TRAILID=%d", trail),
+	}
+	envPrefix := strings.Join(envVars, " ")
 
 	cmd := []string{
+		// Source GO paths.
+		fmt.Sprintf("source ~/.profile"),
+		// Switch to project dir
 		fmt.Sprintf("cd %s", projectDir),
-		fmt.Sprintf("go run cmd/scenario/scenario.go -s %s -c %s -r %d", name, connect, repeat),
 	}
 
-	if err := session.Run(strings.Join(cmd, "; ")); err != nil {
-		log.Fatalf("unable to run scenario: %s", err)
+	// Delete deprecated data.
+	deleteDirs := []string{
+		filepath.Join(simulation, "opp-edge-results"),
+		filepath.Join(simulation, "results"),
+		filepath.Join(simulation, "out"),
 	}
+
+	for _, dir := range deleteDirs {
+		cmd = append(cmd, "rm -rf "+dir)
+	}
+
+	cmd = append(cmd,
+		fmt.Sprintf("%s go run cmd/consumer/opp_edge_run.go -broker %s -path %s -config %s",
+			envPrefix, broker, simulation, filepath.Join(simulation, "opp-edge-config.json")),
+	)
+
+	log.Printf("bashscript:\n%s\n", strings.Join(cmd, "; \n"))
+
+	start := time.Now()
+	if err = session.Run(strings.Join(cmd, "; ")); err != nil {
+		err = fmt.Errorf("unable to run scenario: %s", err)
+		return
+	}
+
+	duration = time.Now().Sub(start)
+
+	return
 }
 
 func startDocker(jobs int) (id string) {
@@ -143,6 +196,10 @@ func startDocker(jobs int) (id string) {
 }
 
 func stop(id string) {
+	if id == "" {
+		return
+	}
+
 	log.Printf("Stopping container %s", id)
 	if err := cli.ContainerStop(context.Background(), id, nil); err != nil {
 		panic(err)
@@ -154,30 +211,84 @@ func stop(id string) {
 	}
 }
 
+func overheadFile(scenario string) (file *os.File, err error) {
+
+	dir := "evaluation/meta"
+	_ = os.MkdirAll(dir, 0755)
+
+	filename := filepath.Join(dir, fmt.Sprintf("overhead-%s.csv", scenario))
+
+	_ = os.Remove(filename)
+
+	return os.Create(filename)
+}
+
+func runEvaluation(connect string, jobs int) {
+	dockerId = startDocker(jobs)
+	defer stop(dockerId)
+
+	// Sleep to ensure that the docker is started and connected.
+	time.Sleep(time.Second * 3)
+
+	scenario := fmt.Sprintf("%sj%dd", string(connect[0]), jobs)
+	file, err := overheadFile(scenario)
+	if err != nil {
+		stop(dockerId)
+		log.Fatalf("unable to create logfile: %s", err)
+	}
+	defer func() { _ = file.Close() }()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	_ = writer.Write([]string{"scenarioId", "trailId", "duration"})
+
+	for inx := 0; inx < repeat; inx++ {
+		duration, err := runScenario(scenario, connect, inx)
+
+		if err != nil {
+			stop(dockerId)
+			log.Fatalln(err)
+		}
+
+		record := []string{
+			scenario, fmt.Sprint(inx), duration.String(),
+		}
+
+		log.Printf("record: %v", record)
+		_ = writer.Write(record)
+		writer.Flush()
+	}
+}
+
 func main() {
 
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	jobNums = *flags.IntSlice("j", jobNums, "set parallel job numbers")
+	connects = *flags.StringSlice("c", connects, "set connection")
+	flag.IntVar(&repeat, "r", repeat, "repeat")
+	flag.Parse()
+
+	initDockerSSH()
 	defer func() {
 		_ = cli.Close()
 		_ = sshClient.Close()
 	}()
 
-	updateRepo()
-
-	jobNums := []int{1, 2, 4, 6, 8}
-	connects := []string{
-		"p2p", "relay",
+	session, err := sshClient.NewSession()
+	if err != nil {
+		log.Fatalf("unable to create a new session: %s", err)
 	}
+	defer func() {
+		_ = session.Close()
+	}()
+
+	updateRepo()
 
 	for _, connect := range connects {
 		for _, jobs := range jobNums {
-			id := startDocker(jobs)
-			// Sleep to ensure that the docker is started and connected.
-			time.Sleep(time.Second * 3)
-
-			name := fmt.Sprintf("%sj%d", string(connect[0]), jobs)
-			runScenario(name, connect, 3)
-
-			stop(id)
+			runEvaluation(connect, jobs)
 		}
 	}
 }
