4b2560844e50efcb272baa8032b3cb7b145b34b5
patrick <patrick@kappa-mm.de>
2021-10-06 12:15:06 +0200

    Check in.

--- cmd/evaluation/scenario.go
+++ cmd/evaluation/scenario.go
@@ -19,15 +19,15 @@ const (
 )
 
 var (
-	defaultJobNums  = []int{1, 2, 4, 6, 8}
-	defaultConnects = []string{"p2p", "relay"}
-	start           int
-	repeat          = 5
-	docker          bool
-	local           bool
-	jobNums         []int
-	connects        []string
-	scenarioName    string
+	defaultJobNums = []int{1, 2, 4, 6, 8}
+	start          int
+	repeat         = 5
+	docker         bool
+	local          bool
+	startWorker    bool
+	jobNums        []int
+	connects       []string
+	scenarioName   string
 )
 
 var cancel context.CancelFunc
@@ -112,25 +112,28 @@ func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
 	nums := flags.IntSlice("j", defaultJobNums, "set parallel job numbers")
-	conns := flags.StringSlice("c", defaultConnects, "set connection")
+	conns := flags.StringSlice("c", []string{"p2p", "relay"}, "set connection")
 	flag.IntVar(&start, "ts", 0, "trail start")
 	flag.IntVar(&repeat, "tr", repeat, "repeat")
 	flag.BoolVar(&docker, "d", false, "use docker")
-	flag.BoolVar(&local, "l", false, "use docker")
+	flag.BoolVar(&local, "l", false, "start from local")
+	flag.BoolVar(&startWorker, "w", false, "start worker")
 	flag.StringVar(&scenarioName, "n", "", "scenario name")
 	flag.Parse()
 
 	jobNums = *nums
 	connects = *conns
 
+	log.Printf("###################################################")
 	log.Printf("jobNums: %v", jobNums)
 	log.Printf("connects: %v", connects)
 	log.Printf("start: %v", start)
 	log.Printf("repeat: %v", repeat)
 	log.Printf("docker: %v", docker)
 	log.Printf("local: %v", local)
-
-	defer cancel()
+	log.Printf("startWorker: %v", startWorker)
+	log.Printf("scenarioName: %v", scenarioName)
+	log.Printf("###################################################")
 
 	worker := scenario.NewWorker(broker)
 
@@ -150,6 +153,19 @@ func main() {
 			Simulation: "~/patrick/project.go.omnetpp/evaluation/tictoc",
 		})
 	}
+
+	if !startWorker {
+		for _, connect := range connects {
+			if err := runEvaluation(runner, connect, 0); err != nil {
+				cancel()
+				log.Fatalln(err)
+			}
+		}
+
+		return
+	}
+
+	//defer cancel()
 
 	for _, jobs := range jobNums {
 		if docker {
