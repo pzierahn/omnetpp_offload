014513d650ee7968d6737f394fb859ee8e867dbe
Patrick Zierahn <5384625+pzierahn@users.noreply.github.com>
2022-10-30 09:57:33 +0100

    Write logs to log file and stderr

--- cmd/run/opp_offload_run.go
+++ cmd/run/opp_offload_run.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/pzierahn/omnetpp_offload/consumer"
 	"github.com/pzierahn/omnetpp_offload/gconfig"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -15,6 +17,7 @@ import (
 var path string
 var configPath string
 var timeout time.Duration
+var writeLog bool
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
@@ -22,9 +25,39 @@ func init() {
 	flag.StringVar(&path, "path", ".", "set simulation path")
 	flag.StringVar(&configPath, "config", "", "set simulation config JSON")
 	flag.DurationVar(&timeout, "timeout", time.Hour*3, "set timeout for execution")
+	flag.BoolVar(&writeLog, "wl", false, "write logs to .cache/evaluation")
+
+	flag.Parse()
+}
+
+func enableLogWrite() {
+
+	date := time.Now()
+	logName := fmt.Sprintf("consumer.%s.%s.log",
+		date.Format("2006-02-01"), date.Format("15-04-05"))
+
+	logDir := filepath.Join(gconfig.CacheDir(), "logs")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		log.Fatalln(err)
+	}
+
+	logPath := filepath.Join(logDir, logName)
+	log.Printf("Write logs to %s", logPath)
+
+	file, err := os.OpenFile(logPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	mw := io.MultiWriter(os.Stderr, file)
+	log.SetOutput(mw)
 }
 
 func main() {
+
+	if writeLog {
+		enableLogWrite()
+	}
 
 	config := gconfig.ParseFlagsBroker()
 
